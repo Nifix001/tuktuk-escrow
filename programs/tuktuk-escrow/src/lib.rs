795b36f1e0b97a10436be189f6d7915675e3028a
Nifix001 <agbajeoluwanifemi20@gmail.com>
2026-02-18 14:43:41 +0100

    update

---
+++ programs/tuktuk-escrow/src/lib.rs
@@ -0,0 +1,30 @@
+#![allow(unexpected_cfgs)]
+#![allow(deprecated)]
+
+use anchor_lang::prelude::*;
+
+mod state;
+mod instructions;
+
+use instructions::*;
+
+declare_id!("3guFRQANk2kcU4LQVekbw7T8iF1jts85FDuz8JzQEjvp");
+
+#[program]
+pub mod tuktuk_escrow {
+    use super::*;
+
+    pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64, expiry: i64,) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive, expiry, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
+    }
+}
